bea167730e8227241bfbb5ab8ffe871dcc8e6c41
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-10-04 11:30:26 +0530

    updated

---
+++ react/index.js
@@ -0,0 +1,2 @@
+var profile1 = { username: "pulkit", age: 21, hobbies: "football" };
+console.log(profile1);
---
+++ react/index.ts
@@ -0,0 +1,7 @@
+interface Profile{
+    username:string,
+    age:number,
+    hobbies:string
+}
+let profile1 : Profile = {username:"pulkit",age:21,hobbies:"football"};
+console.log(profile1);
