eb3490225fe41015c432006cf01e86692e88a520
Pulkit Shringi <pulkitshringi@yahoo.com>
2024-10-05 23:25:19 +0530

    updated

--- Java/Cat.java
+++ Java/Cat.java
@@ -1,5 +1,14 @@
-public class Cat{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+import java.util.Scanner;
+class HelloWorld{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        if(age<18){
+            System.out.println("Teen");
+        }
+        else{
+            System.out.println("Adult");
+        }
+        
     }
-};
+}
