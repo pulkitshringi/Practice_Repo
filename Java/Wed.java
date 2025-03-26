a2646fb6ab96eaf7ba3cea2eaeefd88320661171
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-03-26 19:58:33 +0530

    chore: daily Java Problems - 26 March 25

---
+++ Java/Wed.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Wed {
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       String day = sc.next();
+       System.out.println("Today is " + day);
+       sc.close();
+    }
+}
