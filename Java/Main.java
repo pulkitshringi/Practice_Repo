0ff696e674149d9188530e57ca8742e485548c0d
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-04-11 11:19:26 +0530

    chore : daily java problem

---
+++ Java/Main.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+     Set<Integer> set1 = new HashSet<>();
+
+    }
+}
--- Java/Wed.java
+++
@@ -1,5 +0,0 @@
-class Wed{
-    public static void main(String[] args){
-        System.out.println("Let's get back to work!");
-    }
-}
