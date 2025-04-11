08a4e79f9e9043f0bcf07c5823533b4c32cd88ab
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-04-11 19:23:39 +0530

    chore daily java prob

--- Java/Main.java
+++ Java/Main.java
@@ -1,7 +1,10 @@
 import java.util.*;
 class Main{
     public static void main(String[] args) {
-     Set<Integer> set1 = new HashSet<>();
-
+    TreeSet<Integer> set = new TreeSet<>();
+    set.add(1);
+    set.add(-22);
+    set.add(3);
+    System.out.println(set);
     }
 }
