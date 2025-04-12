f8bf5bc89f88752bdd434005549200dcbed289d9
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-04-12 17:44:45 +0530

    chore: daily java question

--- Java/Main.java
+++ Java/Main.java
@@ -1,10 +1,52 @@
 import java.util.*;
+class Car{
+    String model;
+    int price;
+    Car(String model,int price){
+        this.model = model;
+        this.price = price;
+    };
+};
+class AscendingPooki implements Comparator<Car>{
+    @Override // optional
+    public int compare(Car a,Car b){ // use public
+        return a.price-b.price;
+    }
+};
+class DescendingPooki implements Comparator<Car>{
+    public int compare(Car a, Car b){
+        return b.price-a.price;
+    }
+}
 class Main{
     public static void main(String[] args) {
-    TreeSet<Integer> set = new TreeSet<>();
-    set.add(1);
-    set.add(-22);
-    set.add(3);
-    System.out.println(set);
-    }
+        ArrayList<Car> list = new ArrayList<>();
+        list.add(new Car("tata", 6300));
+        list.add(new Car("mercedes",1200));
+        list.add(new Car("nano",63000));
+
+        System.out.println("Normal : ");
+
+       for(Car l1 : list){
+        System.out.println(l1.model + " : " + l1.price);
+       }
+
+       System.out.println("Ascending : ");
+
+       Collections.sort(list,new AscendingPooki()); // ascending 
+
+       for(Car l1 : list){
+        System.out.println(l1.model + " : " + l1.price);
+       }
+
+       System.out.println("Descending : ");
+
+
+       Collections.sort(list,new DescendingPooki());
+
+       for(Car l1 : list){
+        System.out.println(l1.model + " : " + l1.price);
+       }
+
+}
 }
