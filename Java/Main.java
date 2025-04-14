3b48dc8c34615b705278432723ee5e2b89569b61
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-04-14 22:54:39 +0530

    chore: question solved

--- Java/Main.java
+++ Java/Main.java
@@ -1,9 +1,15 @@
 import java.util.*;
-import java.util.concurrent.PriorityBlockingQueue;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Daily ques");
-        
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> l1 = new ArrayList<>();
+        for(int i=0;i<5;i++){
+            int n = sc.nextInt();
+            l1.add(n);
+        };
+        for(int i = 0;i<5;i++){
+            System.out.println("Your no's are : " + l1.get(i));
+        }
 }
 }
