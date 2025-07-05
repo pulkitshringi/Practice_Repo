e4a7d60da0dd7ef7573185df8fbbf61f018ce97a
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-07-05 15:46:48 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -2,14 +2,10 @@ import java.util.*;
 
 class Main{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> l1 = new ArrayList<>();
-        for(int i=0;i<5;i++){
-            int n = sc.nextInt();
-            l1.add(n);
-        };
-        for(int i = 0;i<5;i++){
-            System.out.println("Your no's are : " + l1.get(i));
-        }
+       int[] arr = new int[5];
+       for (int i = 0; i < arr.length; i++) {
+            arr[i] = 19;
+       }
+       System.out.println(arr[1]);
 }
 }
