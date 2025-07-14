9fd6d42f9a6c990e2011ee3df360841e04ea926c
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-07-15 02:50:01 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -1,30 +1,17 @@
-// 9th July 25
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        int n;
+// 15th July 25
+import java.util.ArrayList;
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter n : ");
-        n = sc.nextInt();
-        Pattern p = new Pattern();
-        p.pattern17(n);
-    }
-}
- class Pattern{
-    public void pattern17(int n) {
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            char ch = 'A';
-            int size = i*2-1;
-            for(int j=1;j<=size;j++){
-                System.out.print(ch);
-                if(j<=size/2) ch++;
-                else ch--;
-            }
-            System.out.println();
-        }
+        char ch ='g';
+        System.out.println(ch);
+       ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(12);
+        arr.add(25);
+        arr.add(36);
+        arr.add(46);
+        arr.remove(1);
+        System.out.println(arr);
     }
 }
