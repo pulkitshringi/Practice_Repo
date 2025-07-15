730f304f1175f23ff14a40d7a01d04133ac84898
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-07-16 00:59:19 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -1,17 +1,19 @@
-// 15th July 25
 import java.util.ArrayList;
 import java.util.Scanner;
 class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char ch ='g';
-        System.out.println(ch);
-       ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(12);
-        arr.add(25);
-        arr.add(36);
-        arr.add(46);
-        arr.remove(1);
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(69);
+        arr.add(42);
+        arr.add(123);
+        arr.add(7979);
         System.out.println(arr);
+        arr.set(2,321);
+        System.out.println(arr);
+        arr.remove(0);
+        System.out.println(arr);
+        arr.add(1,6969);
+        System.out.println(arr);  
     }
 }
