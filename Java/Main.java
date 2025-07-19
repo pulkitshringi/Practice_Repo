5c064bb3ac9d75954fab9f4342e980986af153d2
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-07-20 00:31:05 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -1,19 +1,11 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(69);
-        arr.add(42);
-        arr.add(123);
-        arr.add(7979);
-        System.out.println(arr);
-        arr.set(2,321);
-        System.out.println(arr);
-        arr.remove(0);
-        System.out.println(arr);
-        arr.add(1,6969);
-        System.out.println(arr);  
+        int n = sc.nextInt();
+        while(n>0){
+            System.out.println("I love miti <3");
+            n--;
+        }
     }
 }
