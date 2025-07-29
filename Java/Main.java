649e0730ecfcb1d3d113c47dc1ee301366bdf098
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-07-29 14:24:38 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        while(n>0){
-            System.out.println("I love miti <3");
-            n--;
-        }
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            while(n>0){
+                System.out.println("I love miti <3");
+                n--;
+            }
     }
 }
