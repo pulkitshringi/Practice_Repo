2b8484d069737d10bb3073282ce44c76e7f3cdac
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-08-09 13:47:42 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -1,17 +1,6 @@
-// 4th Aug 25
 import java.util.*;
 class Main{
-    public static void main(String[] args) {
-        HashSet<Integer> hset = new HashSet<>();
-        hset.add(23);
-        hset.add(69);
-        hset.add(43);
-        hset.add(23);
-        hset.add(69);
-        System.out.println(hset);
-        Iterator<Integer> it = hset.iterator();
-        while(it.hasNext()){
-            System.out.print(it.next() + " ");
-        }
+    public static void main(String[] args){
+        System.out.println("Let's restart :)");
     }
 }
