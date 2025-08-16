e489a30988903359fb19ba62ab037d12f4162775
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-08-16 14:56:58 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -1,7 +1,6 @@
-// 14th Aug 25
-import java.util.*;
-class Main{
-    public static void main(String[] args){
-        System.out.println("I'm back - Pulkit S.");
+// 16th Aug 25
+public  class Main{
+    public static void main(String[] args) {
+        System.out.println("i'm checking");
     }
-}
+};
