41587b8fa8266865267fe45bcf798a10cfb53d94
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-08-17 12:05:24 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -1,6 +1,11 @@
-// 16th Aug 25
-public  class Main{
+import java.util.*;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("i'm checking");
+     Map<Integer,String> mpp = new HashMap<>();
+     mpp.put(23,"check");
+     mpp.put(69,"hehehweg");
+     mpp.put(420,"Java bitch here i'm");
+     System.out.println(mpp.get("check"));
     }
-};
+}
