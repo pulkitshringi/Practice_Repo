ff92e8711d25a64a492bf13285b1dca7a4670df8
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-08-30 15:57:21 +0530

    maximum frequency | java

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,21 @@
+import java.util.*;
 class Main{
-    public static void main(String[] args){
-        System.out.println("29 Aug 25");
-    }
+    public static void main(String[] args) {
+        int[] arr = {23,43,69,55,33,23,43,69,69};
+        Map<Integer,Integer> mpp = new HashMap<>();
+        for(int x: arr){
+            mpp.put(x,mpp.getOrDefault(x, 0)+1);
+        }
+        int maxFreq = Integer.MIN_VALUE;
+        int minNo = Integer.MAX_VALUE;
+
+        for(Map.Entry<Integer,Integer> it : mpp.entrySet()){
+            if(it.getValue()>maxFreq) {
+                maxFreq = it.getValue();
+                minNo = it.getKey();
+            }
+            else if(it.getValue()==maxFreq) minNo = Math.min(minNo,it.getKey());
+        }
+        System.out.println("Max Frequency no. is : " + minNo);
+    }   
 }
