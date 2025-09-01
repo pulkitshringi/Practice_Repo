9bf1675206b8cb0fab0f02debb148c7f8149b74c
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-09-01 20:25:37 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,21 @@
+class Solution {    
+    public String largeOddNum(String s) {
+        //your code goes here
+        int j = 0;
+        while(s.charAt(j)=='0' && j<s.length()){
+            j++;
+        }
+
+        for(int i=s.length();i>j;i--){
+            if(Integer.parseInt(s.substring(j,i))%2==1) return s.substring(j,i);
+        }
+        return "0";
+    }
+}
+
 class Main{
     public static void main(String[] args){
-        System.out.println("31 Aug 2025");
+    //    Solution s1 = new Solution();
+    //    System.out.println(s1.largeOddNum("00005"));
     }
 }
