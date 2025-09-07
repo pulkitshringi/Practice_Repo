ce59050e36464c476efbb6b5d8d6fc2aa54a86a4
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-09-07 20:15:52 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -1,16 +1,30 @@
-
-import java.io.*;
-
-// other way of taking input using input stream.
+// Highest Occurring Element in an Array
+import java.util.*;
 class Main{
-    public static void main(String[] args) throws IOException {
-        InputStreamReader io = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(io);
-        int no= Integer.parseInt(br.readLine());
-        float no2=Float.parseFloat(br.readLine());
-        char no3=br.readLine().charAt(0);
-        System.out.println(no);
-        System.out.println(no2);
-        System.out.println(no3);
+  public static void main(String[] args) {
+            Solution s1 = new Solution();
+            int[] nums = {12,33,44,56,43,33,55,55,55};
+            System.out.println(s1.mostFrequentElement(nums));
+  }}
+
+class Solution {
+    public int mostFrequentElement(int[] nums) {
+     Map<Integer,Integer> mpp = new HashMap<>();
+     for(int x:nums){
+        mpp.put(x,mpp.getOrDefault(x,0)+1);
+     }
+     int maxFreq = 0;
+     int minEle = Integer.MAX_VALUE;
+     for(Map.Entry<Integer,Integer> it:mpp.entrySet()){
+        if(it.getValue()>maxFreq){
+            maxFreq = it.getValue();
+            minEle= it.getKey();
+        }
+        else if(it.getValue()==maxFreq){
+            minEle = Math.min(it.getKey(),minEle);
+        }
+     }
+     return minEle;
     }
 }
+
