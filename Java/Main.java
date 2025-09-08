e27133823f29402db5b785abd76d0206ad9c0845
Pulkit Shringi <pulkitshringi@yahoo.com>
2025-09-09 00:59:01 +0530

    updated

--- Java/Main.java
+++ Java/Main.java
@@ -1,29 +1,16 @@
-// Highest Occurring Element in an Array
-import java.util.*;
-class Main{
-  public static void main(String[] args) {
-            Solution s1 = new Solution();
-            System.out.println(s1.mostFrequentElement(new int[]{12,33,44,56,43,33,55,55,55}));
-  }}
-
+// Sum of Highest and Lowest Frequency 
 class Solution {
-    public int mostFrequentElement(int[] nums) {
-     Map<Integer,Integer> mpp = new HashMap<>();
-     for(int x:nums){
-        mpp.put(x,mpp.getOrDefault(x,0)+1);
-     }
-     int maxFreq = 0;
-     int minEle = Integer.MAX_VALUE;
-     for(Map.Entry<Integer,Integer> it:mpp.entrySet()){
-        if(it.getValue()>maxFreq){
-            maxFreq = it.getValue();
-            minEle= it.getKey();
-        }
-        else if(it.getValue()==maxFreq){
-            minEle = Math.min(it.getKey(),minEle);
-        }
-     }
-     return minEle;
+    public int sumHighestAndLowestFrequency(int[] nums) {
+      Map<Integer,Integer> mpp = new HashMap<>();
+      for(int k: nums){
+        mpp.put(k,mpp.getOrDefault(k,0)+1);
+      }
+      int maxFreq = 0;
+      int minFre = Integer.MAX_VALUE;
+      for(Map.Entry<Integer,Integer> it: mpp.entrySet()){
+        minFre = Math.min(minFre,it.getValue());
+        maxFreq = Math.max(maxFreq,it.getValue());
+      }
+      return minFre+maxFreq;
     }
 }
-
